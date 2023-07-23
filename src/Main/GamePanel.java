ee2d2342c8ba9df7bf2d115078513b6cfcb4132b
Hritika Kowlessur <hritika.kowlessur@umail.uom.ac.mu>
2023-07-23 23:49:50 +0400

    added Tile.java and TileManager.java

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import Character.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -22,16 +23,16 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenHeight = tileSize * maxScreenRow; // screen height
 
     int FPS = 60; // frames per second
-
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
 
     // set player's default position
-    //int playerX = 100;
-    //int playerY = 100;
+    // int playerX = 100;
+    // int playerY = 100;
 
-    //int playerSpeed = 4;
+    // int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -39,7 +40,7 @@ public class GamePanel extends JPanel implements Runnable {
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
-        //requestFocus();
+        // requestFocus();
     }
 
     public void startGameThread() {
@@ -77,15 +78,16 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        
-        player.update();        
+
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        //g2.setColor(Color.WHITE);
-        //g2.fillRect(playerX, playerY, tileSize, tileSize);
+        // g2.setColor(Color.WHITE);
+        // g2.fillRect(playerX, playerY, tileSize, tileSize);
+        tileM.draw(g2); //tile is drawn before player so as to be behind the player
         player.draw(g2);
         g2.dispose();
     }
