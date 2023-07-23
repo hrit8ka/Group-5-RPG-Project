3cdc4f16642a9eb82310c0379b06d476c7954cd6
Hritika Kowlessur <hritika.kowlessur@umail.uom.ac.mu>
2023-07-24 02:17:16 +0400

    adding collisions

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -37,11 +37,13 @@ public class TileManager {
             fis = new FileInputStream(file);
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(fis);
+            tile[1].collision = true;
 
             file = new File("src/tiles/water.png");
             fis = new FileInputStream(file);
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(fis);
+            tile[2].collision = true;
 
             file = new File("src/tiles/earth.png");
             fis = new FileInputStream(file);
@@ -52,6 +54,7 @@ public class TileManager {
             fis = new FileInputStream(file);
             tile[4] = new Tile();
             tile[4].image = ImageIO.read(fis);
+            tile[4].collision = true;
 
             file = new File("src/tiles/sand.png");
             fis = new FileInputStream(file);
@@ -104,10 +107,8 @@ public class TileManager {
 
             int worldX = worldCol * gp.tileSize;
             int worldY = worldRow * gp.tileSize;
-            int screenX = worldX - gp.player.worldX + gp.player.screenX; // screenX is the x coordinate of the tile on
-                                                                         // the screen
-            int screenY = worldY - gp.player.worldY + gp.player.screenY; // screenY is the y coordinate of the tile on
-                                                                         // the screen
+            int screenX = worldX - gp.player.worldX + gp.player.screenX;
+            int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
             if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
                     worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
