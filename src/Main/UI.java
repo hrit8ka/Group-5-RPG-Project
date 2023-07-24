74cb60a1fe898635c200965322ec6ffc236bada1
Hritika Kowlessur <hritika.kowlessur@umail.uom.ac.mu>
2023-07-24 20:35:37 +0400

    first steps of RPG completed

--- src/Main/UI.java
+++ src/Main/UI.java
@@ -9,7 +9,7 @@ import Object.OBJ_Key;
 
 public class UI {
     GamePanel gp;
-    Font arial_40;
+    Font arial_40, arial_80B;
     BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
@@ -20,6 +20,7 @@ public class UI {
         this.gp = gp;
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+        arial_80B = new Font("Arial", Font.BOLD, 80);
         OBJ_Key key = new OBJ_Key();
         keyImage = key.image;
     }
@@ -30,21 +31,51 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
-        g2.setFont(arial_40);
-        g2.setColor(Color.white);
-        g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
-        g2.drawString("x " + gp.player.hasKey, 74, 65);
 
-        // message
-        if (messageOn == true) {
-            g2.setFont(g2.getFont().deriveFont(30F));
-            g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
+        if (gameFinished == true) {
+            g2.setFont(arial_40);
+            g2.setColor(Color.white);
 
-            messageCounter++;
+            String text;
+            int textLength;
+            int x;
+            int y;
 
-            if (messageCounter > 120) {
-                messageCounter = 0;
-                messageOn = false;
+            text = "You found the treasure chest!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 - (gp.tileSize * 3);
+            g2.drawString(text, x, y);
+            g2.setFont(arial_80B);
+            g2.setColor(Color.cyan);
+
+            text = "CONGRATULATIONS!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 + (gp.tileSize * 2);
+
+            gp.gameThread = null;
+
+        } else {
+
+            g2.setFont(arial_40);
+            g2.setColor(Color.white);
+            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
+            g2.drawString("x " + gp.player.hasKey, 74, 65);
+
+            // message
+            if (messageOn == true) {
+                g2.setFont(g2.getFont().deriveFont(30F));
+                g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
+
+                messageCounter++;
+
+                if (messageCounter > 120) {
+                    messageCounter = 0;
+                    messageOn = false;
+                }
             }
         }
 
