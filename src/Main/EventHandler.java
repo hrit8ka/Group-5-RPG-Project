f8be1c4dfaedf8d3cb12d6aaaf2c5891e4c9da3f
Hritika Kowlessur <hritika.kowlessur@umail.uom.ac.mu>
2023-08-01 16:27:11 +0400

    working on events

--- src/Main/EventHandler.java
+++ src/Main/EventHandler.java
@@ -1,45 +1,58 @@
 package Main;
 
-import java.awt.Rectangle;
-
 public class EventHandler {
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    // Rectangle eventRect;
+    // int eventRectDefaultX, eventRectDefaultY;
+
+    EventRectangle eventRect[][];
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
+        eventRect = new EventRectangle[gp.maxWorldCol][gp.maxWorldRow];
 
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        int col = 0;
+        int row = 0;
+        while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+            eventRect[col][row] = new EventRectangle();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 2;
+            eventRect[col][row].height = 2;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if (col == gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
+        }
     }
 
     public void checkEvent() {
-        /*if (hit(27, 16, "right") == true) {
-            damagePit(gp.dialogueState);
-        }*/
+        
+        if (hit(27, 16, "right") == true) {
+        damagePit(27, 16, gp.dialogueState);
+        }
+        
         if (hit(23, 12, "up") == true) {
-            healingPool(gp.dialogueState);
+            healingPool(23, 12, gp.dialogueState);
         }
-        if(hit(27,16,"right") == true){
-            teleport(gp.dialogueState);
-        }
+        //if (hit(27, 16, "right") == true) {
+           // teleport(gp.dialogueState);
+        //}
 
     }
 
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
         boolean hit = false;
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if (gp.player.solidArea.intersects(eventRect)) {
+        if (gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
             }
@@ -47,31 +60,32 @@ public class EventHandler {
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
 
-    public void teleport(int gameState){
+    public void teleport(int gameState) {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You found a teleporter! You are teleported!";
         gp.player.worldX = gp.tileSize * 37;
         gp.player.worldY = gp.tileSize * 10;
     }
 
-    public void damagePit(int gameState) {
+    public void damagePit(int col, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You fell into a pit! You lost 1 HP!";
         gp.player.life -= 1; // player loses 1 HP
+        //eventRect[col][row].eventDone = true; // event is done
     }
 
-    public void healingPool(int gameState) {
+    public void healingPool(int col, int row, int gameState) {
         if (gp.keyH.enterPressed == true) {
             gp.gameState = gameState;
             gp.ui.currentDialogue = "You found a healing pool! You are healed!";
             gp.player.life = gp.player.maxLife; // player recovers all HP
         }
-        //gp.keyH.enterPressed = false;
+        // gp.keyH.enterPressed = false;
     }
 }
